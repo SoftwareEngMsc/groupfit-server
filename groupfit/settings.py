2b0adb8c8e5be0b75ac34c66d97d991c86b28fff
OluwatoyinAguiyi <38816490+OluwatoyinAguiyi@users.noreply.github.com>
2024-06-15 15:57:50 +0100

    updated dependencies

--- groupfit/settings.py
+++ groupfit/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'groupfitness',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 INTERNAL_IPS = [
-  '127.0.0.1',
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'groupfit.urls'
