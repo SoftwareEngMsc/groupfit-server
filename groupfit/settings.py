069356c9dac9531076a8482e65dab6da2094078a
OluwatoyinAguiyi <38816490+OluwatoyinAguiyi@users.noreply.github.com>
2024-06-15 16:36:53 +0100

    renamed core app

--- groupfit/settings.py
+++ groupfit/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'groupfitness',
+    'core',
     # 'debug_toolbar',
 ]
 
