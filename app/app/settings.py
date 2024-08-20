ae3279e9bc23c3b45a47b0a65acb18c4a9deaccc
OluwatoyinAguiyi <38816490+OluwatoyinAguiyi@users.noreply.github.com>
2024-08-20 15:17:44 +0100

    fixing server error

--- app/app/settings.py
+++ app/app/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-MEDIA_ROOT = 'vol/web/media'
-STATIC_ROOT = 'vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
